8f3054b28d80946f4225bedc4321094a0f7799cd
Leni <leni.jenschke@wavelens.io>
2024-09-12 17:03:02 +0000

    add global variables and download page

--- frontend/dashboard/urls.py
+++ frontend/dashboard/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("workflow", views.workflow, name="workflow"),
     path("log", views.log, name="log"),
+    path("download", views.download, name="download"),
 ]
