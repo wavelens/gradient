9373d22011ca46f28df5ac19bc5faebe39931c39
Dennis Wuitz <dennish@wuitz.de>
2024-11-06 22:09:03 +0100

    add building scheduler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,9 @@ pub mod model;
 pub mod requests;
 pub mod tables;
 pub mod types;
+pub mod executer;
+pub mod scheduler;
+pub mod input;
 
 use axum::routing::{get, post};
 use axum::Router;
@@ -14,32 +17,43 @@ use sea_orm_migration::prelude::*;
 use types::*;
 
 
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let server_url = env::var("GRADIENT_IP").unwrap_or("127.0.0.1".to_string());
-    let db_url = env::var("GRADIENT_DATABASE_URL").expect("GRADIENT_DATABASE_URL must be set");
+    tokio::spawn(scheduler::schedule_project_loop());
+    tokio::spawn(scheduler::schedule_build_loop());
 
-    let db = Database::connect(db_url)
-        .await
-        .expect("Failed to connect to database");
-    Migrator::up(&db, None).await.unwrap();
-
-    let state = AppState {
-        conn: db,
-    };
-
-    let app = Router::new()
-        .route("/project", get(endpoints::get_projects).post(endpoints::post_projects))
-        .route("/project/:project", get(endpoints::get_project).post(endpoints::post_project))
-        .route("/jobset/:jobset", get(endpoints::get_jobset).post(endpoints::post_jobset))
-        .route("/build/:build", get(endpoints::get_build).post(endpoints::post_build))
-        .route("/user/:user", get(endpoints::get_user).post(endpoints::post_user))
-        .with_state(state);
-
-    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
-    axum::serve(listener, app).await?;
+    serve_web().await?;
 
     Ok(())
+}
+
+async fn serve_web() -> std::io::Result<()> {
+    // TODO: use clap
+    let server_ip = env::var("GRADIENT_IP").unwrap_or("127.0.0.1".to_string());
+    let server_port = env::var("GRADIENT_PORT").unwrap_or("3000".to_string());
+        let server_url = format!("{}:{}", server_ip, server_port);
+        let db_url = env::var("GRADIENT_DATABASE_URL").expect("GRADIENT_DATABASE_URL must be set");
+
+    // let db = Database::connect(db_url)
+    //     .await
+    //     .expect("Failed to connect to database");
+    // Migrator::up(&db, None).await.unwrap();
+
+    // let state = AppState {
+    //     conn: db,
+    // };
+
+    let app = Router::new()
+        .route("/organization", get(endpoints::get_organizations).post(endpoints::post_organizations))
+        .route("/organization/:organization", get(endpoints::get_organization).post(endpoints::post_project))
+        .route("/project/:project", get(endpoints::get_project).post(endpoints::post_project))
+        .route("/build/:build", get(endpoints::get_build).post(endpoints::post_build))
+        .route("/user/:user", get(endpoints::get_user).post(endpoints::post_user))
+        .route("/server", get(endpoints::get_servers).post(endpoints::post_servers));
+
+        // .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
+    axum::serve(listener, app).await
 }
 
---
+++ backend/src/scheduler.rs
@@ -0,0 +1,165 @@
+use std::time::Duration;
+use uuid::Uuid;
+use tokio::time;
+use tokio::task::JoinHandle;
+
+use super::input;
+use super::tables::*;
+use super::executer::*;
+
+pub async fn schedule_project_loop() {
+    let mut current_schedules = vec![];
+    let mut interval = time::interval(Duration::from_secs(5));
+
+    loop {
+        let mut added_schedule = false;
+        current_schedules.retain(|schedule: &JoinHandle<()>| !schedule.is_finished());
+
+        // TODO: look at tokio semaphore
+        while current_schedules.len() < 1 {
+            let project = get_next_project().await;
+            let schedule = tokio::spawn(schedule_project(project));
+            current_schedules.push(schedule);
+            added_schedule = true;
+        }
+
+        if !added_schedule {
+            interval.tick().await;
+        }
+    }
+}
+
+pub async fn schedule_project(project: Project) {
+    println!("Reviewing Project: {}", project.name);
+
+    let has_update = check_project_updates(&project);
+    if !has_update { return; }
+
+    let can_evaluate = evaluate_project(&project);
+    // TODO: Register the project can't be evaluated
+    if !can_evaluate { return; }
+
+    let builds = get_project_rebuilds(&project);
+
+    for build in builds {
+        register_build(&build);
+    }
+}
+
+pub async fn schedule_build_loop() {
+    let mut current_schedules = vec![];
+    let mut interval = time::interval(Duration::from_secs(5));
+
+    loop {
+        let mut added_schedule = false;
+        current_schedules.retain(|schedule: &JoinHandle<()>| !schedule.is_finished());
+
+        while current_schedules.len() < 1 {
+            let build = get_next_build().await;
+
+            if let Some(server) = get_available_server(&build) {
+                let schedule = tokio::spawn(schedule_build(build, server));
+                current_schedules.push(schedule);
+                added_schedule = true;
+            } else {
+                // TODO: More elegant way to handle this
+                register_build(&build);
+            }
+        }
+
+        if !added_schedule {
+            interval.tick().await;
+        }
+    }
+}
+
+pub async fn schedule_build(build: Build, server: Server) {
+    println!("Executing Build: {}", build.id);
+
+    let server_addr = input::url_to_addr(server.url.as_str()).unwrap();
+
+    let mut local_daemon = get_local_store().await;
+    let mut server_daemon = connect(server_addr).await.unwrap();
+
+    println!("Connected to server: {}", server.url);
+
+    // TODO: Change this to Uuid to build
+    // https://docs.rs/nix-daemon/latest/nix_daemon/trait.Store.html#tymethod.query_missing
+    let deps = get_next_build().await; // dummy
+    let dependencies = vec![&deps];
+
+    // TODO: somewhere else
+    // let missing_dependencies = get_missing_builds(&build, &mut server_daemon).await.unwrap();
+
+    copy_builds(dependencies, &mut local_daemon, &mut server_daemon, server_addr, false).await;
+
+    execute_build(vec![&build], &mut server_daemon).await;
+
+    copy_builds(vec![&build], &mut server_daemon, &mut local_daemon, server_addr, true).await;
+}
+
+pub async fn get_next_project() -> Project {
+    // dummy
+    Project {
+        id: Uuid::new_v4(),
+        organization_id: Uuid::nil(),
+        name: "Project Title".to_string(),
+        description: "Project Description".to_string(),
+        last_check_at: 0,
+        created_by: Uuid::nil(),
+        created_at: 0,
+    }
+}
+
+pub fn check_project_updates(project: &Project) -> bool {
+    // dummy
+    println!("Checking for updates on project: {}", project.name);
+    true
+}
+
+pub fn evaluate_project(project: &Project) -> bool {
+    // dummy
+    println!("Evaluating project: {}", project.name);
+    true
+}
+
+pub fn get_project_rebuilds(project: &Project) -> Vec<Build> {
+    // dummy
+    vec![
+        Build {
+            id: Uuid::new_v4(),
+            project_id: project.id,
+            path: "/nix/store/l2kvr9vx55cc469r3ncg04jf425alh3p-tig-2.5.10".to_string(),
+            dependencies: vec![],
+            created_at: 0,
+        }
+    ]
+}
+
+pub fn register_build(build: &Build) {
+    // dummy
+    println!("Registering build: {}", build.id);
+}
+
+pub async fn get_next_build() -> Build {
+    // dummy
+    Build {
+        id: Uuid::new_v4(),
+        project_id: Uuid::nil(),
+        path: "/nix/store/l2kvr9vx55cc469r3ncg04jf425alh3p-tig-2.5.10".to_string(),
+        dependencies: vec![],
+        created_at: 0,
+    }
+}
+
+pub fn get_available_server(build: &Build) -> Option<Server> {
+    // dummy
+    Some(Server {
+        id: Uuid::new_v4(),
+        organization_id: Uuid::nil(),
+        url: "127.0.0.1:22".to_string(),
+        connected: true,
+        last_connection_at: 0,
+        created_at: 0,
+    })
+}
