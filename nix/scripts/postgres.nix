cbb86ad9f80bb706f662b2789b6d40abe83a3f74
Dennis Wuitz <dennish@wuitz.de>
2024-11-07 02:00:02 +0100

    update postgresql dev setup

---
+++ nix/scripts/postgres.nix
@@ -0,0 +1,30 @@
+{ pkgs, stdenv, ... }: let
+  psql = pkgs.postgresql_17;
+in
+stdenv.mkDerivation {
+  pname = "start-postgres";
+  version = "0";
+
+  src = pkgs.fetchurl {
+    executable = true;
+    url = "https://raw.githubusercontent.com/sapcc/keppel/refs/heads/master/testing/with-postgres-db.sh";
+    hash = "sha256-zso9toCgwDwkzHga01gEYPCNYg6GeAp4JcdnmPBgopM=";
+  };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/start-postgres
+
+    substituteInPlace $out/bin/start-postgres --replace pg_ctl ${psql}/bin/pg_ctl
+    substituteInPlace $out/bin/start-postgres --replace initdb ${psql}/bin/initdb
+
+    cat $out/bin/start-postgres
+
+    chmod +x $out/bin/start-postgres
+  '';
+
+  buildInputs = [ pkgs.bash psql ];
+
+}
