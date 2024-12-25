756a8c5b6e0bcaba146b28d2359b23fe3b70328c
Leni Jenschke <leni.jenschke@wavelens.io>
2024-12-25 18:08:06 +0000

    add forms

--- frontend/dashboard/forms.py
+++ frontend/dashboard/forms.py
@@ -4,8 +4,6 @@
 
 from django import forms
 
-from django import forms
-
 class LoginForm(forms.Form):
     username = forms.CharField(
         label='E-Mail-Adresse oder Benutzername',
@@ -28,4 +26,193 @@ class LoginForm(forms.Form):
         widget=forms.CheckboxInput(attrs={
             'class': 'form-check-input'
         })
+    )
+
+
+GEEKS_CHOICES =(
+    ("1", "One"), 
+    ("2", "Two"), 
+    ("3", "Three"), 
+    ("4", "Four"), 
+    ("5", "Five"), 
+) 
+
+class newOrganizationForm(forms.Form):
+    # owner = forms.ChoiceField(
+    #     choices=GEEKS_CHOICES,
+    #     required=True,
+    #     widget=forms.Select,
+    #     label='Besitzer'
+    # )
+    organization_name = forms.CharField(
+        label='Organisations-Name',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    description = forms.CharField(
+        label='Beschreibung',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    # show = forms.BooleanField(
+    #     label='In privates Repository umwandeln',
+    #     required=False,
+    #     widget=forms.CheckboxInput(attrs={
+    #         'class': 'form-check-input'
+    #     })
+    # )
+    # description = forms.CharField(
+    #     label="Beschreibung",
+    #     required=False,
+    #     widget=forms.Textarea(attrs={
+    #         'placeholder': 'Gib eine kurze Beschreibung an (optional)',
+    #         'rows': 4,
+    #     })
+    # )
+    # template = forms.CharField(
+    #     label='Template',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'type': 'search',
+    #         'class': 'form-control',
+    #         'placeholder': 'Vorlage auswählen'
+    #     })
+    # )
+    # issue_label = forms.CharField(
+    #     label='Issue Label',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'class': 'form-control',
+    #         'placeholder': 'Wähle ein Issue-Label-Set.'
+    #     })
+    # )
+    # gitignore = forms.CharField(
+    #     label='.gitignore',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'type': 'search',
+    #         'class': 'form-control',
+    #         'placeholder': 'Wähle eine .gitignore-Vorlage aus.'
+    #     })
+    # )
+    # license = forms.CharField(
+    #     label='Lizenz',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'type': 'search',
+    #         'class': 'form-control',
+    #         'placeholder': 'Wähle eine Lizenz aus.'
+    #     })
+    # )
+    # readme = forms.CharField(
+    #     label='README',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'type': 'search',
+    #         'class': 'form-control',
+    #     })
+    # )
+    # initialisieren = forms.BooleanField(
+    #     label='Repository initalisieren (Fügt .gitignore, License und README-Dateien hinzu)',
+    #     required=False,
+    #     widget=forms.CheckboxInput(attrs={
+    #         'class': 'form-check-input'
+    #     })
+    # )
+    # branch = forms.CharField(
+    #     label='Standardbranch',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'class': 'form-control',
+    #         'value': 'main'
+    #     })
+    # )
+    # format = forms.CharField(
+    #     label='Objektformat',
+    #     required=False,
+    #     widget=forms.TextInput(attrs={
+    #         'type': 'search',
+    #         'class': 'form-control',
+    #         'placeholder': 'sha1'
+    #     })
+    # )
+    # template_check = forms.BooleanField(
+    #     label='Repository zu einem Template machen',
+    #     required=False,
+    #     widget=forms.CheckboxInput(attrs={
+    #         'class': 'form-check-input'
+    #     })
+    # )
+
+class newProjectForm(forms.Form):
+    project_name = forms.CharField(
+        label='Projekt-Name',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    description = forms.CharField(
+        label='Beschreibung',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    repo = forms.ChoiceField(
+        choices=GEEKS_CHOICES,
+        required=True,
+        widget=forms.Select,
+        label='Repository'
+    )
+
+class newServerForm(forms.Form):
+    organization_id = forms.CharField(
+        label='Organization',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    server_name = forms.CharField(
+        label='Server-Name',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    host = forms.CharField(
+        label='Host',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    port = forms.ChoiceField(
+        choices=GEEKS_CHOICES,
+        required=True,
+        widget=forms.Select,
+        label='Port'
+    )
+    username = forms.ChoiceField(
+        choices=GEEKS_CHOICES,
+        required=True,
+        widget=forms.Select,
+        label='Username'
+    )
+    architectures = forms.ChoiceField(
+        choices=GEEKS_CHOICES,
+        required=True,
+        widget=forms.Select,
+        label='Architectures'
+    )
+    features = forms.ChoiceField(
+        choices=GEEKS_CHOICES,
+        required=True,
+        widget=forms.Select,
+        label='Features'
     )
