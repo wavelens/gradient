5104908723be0b424930f703db9c098f0e9a7abd
Leni Jenschke <leni.jenschke@wavelens.io>
2025-01-22 18:45:55 +0000

    complete api.py

--- frontend/dashboard/api.py
+++ frontend/dashboard/api.py
@@ -45,8 +45,44 @@ def register(request, username, name, email, password):
 def login(loginname, password):
     return get_client(None, "user/login", body={'loginname': loginname, 'password': password})
 
+def logout(request):
+    return get_client(request.user, "user/logout", post=True)
+
 def post_organization(request, name, description, use_nix_store):
     return get_client(request.user, "organization", body={'name': name, 'description': description, 'use_nix_store': use_nix_store})
 
+def get_organization(request, organization_id):
+    return get_client(request.user, f"organization/{organization_id}", post=False)
+
 def get_user(request, uid):
     return get_client(request.user, f"user/settings/{uid}", post=False)
+
+def post_project(request, name, description, repository, evaluation_wildcard):
+    return get_client(request.user, "project", body={'name': name, 'description': description, 'repository': repository, 'evaluation_wildcard': evaluation_wildcard})
+
+def get_project(request, project_id):
+    return get_client(request.user, f"project/{project_id}", post=False)
+
+def post_server(request, organization_id, name, host, port, username, architectures, features):
+    return get_client(request.user, "server", body={ 'organization_id': organization_id, 'name': name, 'host': host, 'port': port, 'username': username, 'architectures': architectures, 'features': features})
+
+def get_servers(request):
+    return get_client(request.user, "server", post=False)
+
+def post_build(request, log_streaming):
+    return get_client(request.user, "build", body={'log_streaming': log_streaming})
+
+def get_build(request, build_id):
+    return get_client(request.user, f"build/{build_id}", post=False)
+
+def post_api_key(request, name):
+    return get_client(request.user, "user/api", body={'name': name})
+
+def post_project_check_repository(request, project_id):
+    return get_client(request.user, f"project/{project_id}/check-repository", post=True)
+
+def post_organization_ssh(request, organization_id):
+    return get_client(request.user, f"organization/{organization_id}/ssh", post=True)
+
+def post_server_check(request, server_id):
+    return get_client(request.user, f"server/{server_id}/check", post=True)
