f3e4aa1a7ad4dbf929ae43f1647374db55ef3729
Dennis Wuitz <dennish@wuitz.de>
2026-01-04 22:45:51 +0100

    SPDX-Header: 2025 -> 2026

--- backend/web/src/endpoints/mod.rs
+++ backend/web/src/endpoints/mod.rs
@@ -1,5 +1,5 @@
 /*
-* SPDX-FileCopyrightText: 2025 Wavelens GmbH <info@wavelens.io>
+* SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
 *
 * SPDX-License-Identifier: AGPL-3.0-only
 */
--- nix/tests/gradient/frontend/test-selenium.py
+++ nix/tests/gradient/frontend/test-selenium.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 Wavelens GmbH <info@wavelens.io>
+# SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
