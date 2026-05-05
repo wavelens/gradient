3a41dae848218276734d1fd6fef0aa0c0621290b
Dennis Wuitz <dennish@wuitz.de>
2026-05-06 01:50:53 +0200

    entity: add project_trigger model

---
+++ backend/entity/src/project_trigger.rs
@@ -0,0 +1,40 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+use chrono::NaiveDateTime;
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use crate::ids::{ProjectId, ProjectTriggerId};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize, Serialize)]
+#[sea_orm(table_name = "project_trigger")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: ProjectTriggerId,
+    pub project: ProjectId,
+    /// 0 = polling, 1 = reporter_push, 2 = reporter_pull_request, 3 = time
+    pub trigger_type: i16,
+    /// 0 = hard_abort, 1 = soft_abort, 2 = allow, 3 = skip
+    pub concurrency: i16,
+    pub config: serde_json::Value,
+    pub active: bool,
+    pub last_fired_at: Option<NaiveDateTime>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::project::Entity",
+        from = "Column::Project",
+        to = "super::project::Column::Id"
+    )]
+    Project,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
