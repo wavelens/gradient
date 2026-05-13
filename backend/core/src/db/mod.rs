2d372e7cb1986dbf7a61ee6f9d340968325fb39e
Dennis Wuitz <dennish@wuitz.de>
2026-05-13 21:57:05 +0200

    core: db: add cache_reach helper with direct-overlap support

--- backend/core/src/db/mod.rs
+++ backend/core/src/db/mod.rs
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+pub mod cache_reach;
 pub mod connection;
 pub mod dependency_graph;
 pub mod derivation;
@@ -11,6 +12,7 @@ pub mod drv_output_spec;
 pub mod gc;
 pub mod status;
 
+pub use self::cache_reach::*;
 pub use self::connection::*;
 pub use self::dependency_graph::*;
 pub use self::derivation::*;
