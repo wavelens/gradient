c41dcaf312f4e6371504b19de9281178069fd69e
Dennis Wuitz <dennish@wuitz.de>
2026-06-01 15:22:10 +0200

    feat(core): add cache::ingest module types

---
+++ backend/core/src/cache/mod.rs
@@ -0,0 +1,6 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+pub mod ingest;
--- backend/core/src/lib.rs
+++ backend/core/src/lib.rs
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+pub mod cache;
 pub mod ci;
 pub mod constants;
 pub mod db;
