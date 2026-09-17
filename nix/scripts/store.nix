85c7aafd5cf6d39e6999250997c48385cad2d211
Dennis Wuitz <dennish@wuitz.de>
2026-09-17 02:13:20 +0200

    nix: test: stop seeding busybox, an unbuildable busybox is what proves the relay

--- nix/scripts/store.nix
+++ nix/scripts/store.nix
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-{ pkgs, skipDirectories ? true, package ? pkgs.hello, ... }: let
-  testPkgs = package;
+{ pkgs, skipDirectories ? true, ... }: let
+  testPkgs = pkgs.hello;
   closureInfo = pkgs.stdenvNoCC.mkDerivation {
     name = "closure-info";
 
